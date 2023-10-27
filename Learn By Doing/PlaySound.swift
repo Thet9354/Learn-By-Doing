ea70b4739514f1701067758c7aa9eab5a3fe9645
PhoonThetPine <thetpine254@gmail.com>
2023-10-27 11:32:43 +0800

    Learn By Doing - V2

--- Learn By Doing/PlaySound.swift
+++ Learn By Doing/PlaySound.swift
@@ -5,4 +5,20 @@
 //  Created by Phoon Thet Pine on 27/10/23.
 //
 
+import AVFoundation
 import Foundation
+
+// MSRK: - AUDIO PLAYER
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+        audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+        audioPlayer?.play()
+    } catch {
+        print("Could not find and play the sound file")
+    }
+    }
+}
