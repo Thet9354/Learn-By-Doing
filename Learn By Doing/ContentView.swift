d26c5aa0e8ecfe1909016ac928c8657bd8ff169a
PhoonThetPine <thetpine254@gmail.com>
2023-10-27 09:00:10 +0800

    Initial Commit

---
+++ Learn By Doing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learn By Doing
+//
+//  Created by Phoon Thet Pine on 27/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by Phoon Thet Pine on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
